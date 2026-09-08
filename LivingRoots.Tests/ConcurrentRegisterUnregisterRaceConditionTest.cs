7b5dda28dad53f059067a366fc950b63dc82aba0
Luis <lfgranjaead@gmail.com>
2026-09-08 01:12:34 -0400

    test: update ConcurrentRegisterUnregisterRaceConditionTest for new constructor parameters

--- LivingRoots.Tests/ConcurrentRegisterUnregisterRaceConditionTest.cs
+++ LivingRoots.Tests/ConcurrentRegisterUnregisterRaceConditionTest.cs
@@ -43,7 +43,8 @@ namespace LivingRoots.Tests
 
             // Create a single ModController instance to be shared across all tasks
             var controller = new ModController(_mockHelper.Object, _mockMonitor.Object, _mockManifest.Object,
-                _mockSoilHealthService.Object, _mockSaveIdProvider.Object);
+                _mockSoilHealthService.Object, _mockSaveIdProvider.Object,
+                new Mock<ICompostingBinService>().Object, new Mock<ISoilDecayService>().Object);
 
             // First register events to set up the controller with handlers
             controller.RegisterEvents();
@@ -142,7 +143,8 @@ namespace LivingRoots.Tests
 
             // Create a single ModController instance to be shared across all tasks
             var controller = new ModController(_mockHelper.Object, _mockMonitor.Object, _mockManifest.Object,
-                _mockSoilHealthService.Object, _mockSaveIdProvider.Object);
+                _mockSoilHealthService.Object, _mockSaveIdProvider.Object,
+                new Mock<ICompostingBinService>().Object, new Mock<ISoilDecayService>().Object);
 
             // First register events to set up the controller with handlers
             controller.RegisterEvents();
